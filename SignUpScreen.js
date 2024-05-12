48080c40c904ab351fef6d86182fe071e803c925
juniot123 <133945745+juniot123@users.noreply.github.com>
2024-05-12 23:34:51 +0530

    Add files via upload

--- SignUpScreen.js
+++ SignUpScreen.js
@@ -1,8 +1,11 @@
-// SignUpScreen.js
 import React from 'react';
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, TextInput, Button, Text, StyleSheet } from 'react-native';
 
-const SignUpScreen = () => {
+const SignUpScreen = ({ navigation }) => {
+  const handleSignUp = () => {
+    navigation.navigate('SignUp');
+  };
+
   return (
     <View style={styles.container}>
       <TextInput
@@ -17,12 +20,17 @@ const SignUpScreen = () => {
       <TextInput
         style={styles.input}
         placeholder="Email"
-        keyboardType="email-address"
+        secureTextEntry
       />
-      <Button
-        title="Sign Up"
-        onPress={() => alert('Sign up functionality goes here')}
-      />
+      
+      <View style={styles.signupContainer}>
+        <Button
+          title="Sign Up"
+          onPress={handleSignUp}
+          style={[styles.button, { width: '100%' }]} // Adjust width to 100%
+          color="black" // Change button color to black
+        />
+      </View>
     </View>
   );
 };
@@ -32,15 +40,27 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    backgroundColor: 'rgba(173, 216, 230, 0.5)', // Light transparent blue
   },
   input: {
     width: '80%',
     marginBottom: 15,
     padding: 10,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: 'skyblue', // Set border color as sky blue
     borderRadius: 5,
+    backgroundColor: 'skyblue', // Change background color to sky blue
+  },
+  signupContainer: {
+    marginTop: 60, // Adjust margin top as needed
+    alignItems: 'left',
+  },
+  signupText: {
+    marginBottom: 10, // Adjust margin bottom as needed
+    color: '#333',
+  },
+  button: {
+    marginBottom: 35, // Add margin bottom to the buttons for spacing
   },
 });
-
 export default SignUpScreen;
